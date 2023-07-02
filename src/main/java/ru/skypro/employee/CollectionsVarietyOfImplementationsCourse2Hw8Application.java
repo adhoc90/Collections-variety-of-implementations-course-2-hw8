166b8d76ac347fcb6945c27bb87fdb121c671396
ad_ho <ad_hoc90@mail.ru>
2023-07-02 17:45:57 +0300

    initial commit

---
+++ src/main/java/ru/skypro/employee/CollectionsVarietyOfImplementationsCourse2Hw8Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsVarietyOfImplementationsCourse2Hw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsVarietyOfImplementationsCourse2Hw8Application.class, args);
+    }
+
+}
