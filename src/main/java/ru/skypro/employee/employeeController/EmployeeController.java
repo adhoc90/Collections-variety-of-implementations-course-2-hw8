abc2da186b9f60121e539fe9dc813260bc4c83ec
ad_ho <ad_hoc90@mail.ru>
2023-07-02 19:57:52 +0300

    initial commit

---
+++ src/main/java/ru/skypro/employee/employeeController/EmployeeController.java
@@ -0,0 +1,44 @@
+package ru.skypro.employee.employeeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.employee.employeeService.EmployeeService;
+import ru.skypro.employee.model.Employee;
+
+import java.util.Collection;
+
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee createEmployee(@RequestParam("firstName") String firstName,
+                                   @RequestParam("lastName") String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam("firstName") String firstName,
+                                   @RequestParam("lastName") String lastName) {
+        return employeeService.deleteEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam("firstName") String firstName,
+                                 @RequestParam("lastName") String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/list")
+    public Collection<Employee> showEmployeeList() {
+        return employeeService.showEmployeeList();
+    }
+}
